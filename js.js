ad8dfb7c5562b5bd1e47ed903fd685f461bcab9c
Peter Samuel <85273974+ZedKaze@users.noreply.github.com>
2023-09-17 15:43:46 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,76 @@
+// back to top and header links transitions
+
+document.addEventListener('DOMContentLoaded', function () {
+    var headerLink = document.getElementById("backToTop");
+    headerLink.addEventListener('click', function (event) {
+        event.preventDefault(); // prevent the default behavior of jumping to the section
+
+        var section = document.querySelector(event.target.getAttribute('href'));
+        var scrollOffset = 0; // adjust this value according to your needs
+
+        window.scrollTo({
+            top: section.offsetTop - scrollOffset,
+            behavior: 'smooth' // adds smooth scrolling effect
+        });
+    });
+});
+
+let span = document.getElementById("backToTop");
+
+window.onscroll = function () {
+    this.scrollY >= 500 ? span.classList.add("show") : span.classList.remove("show");
+};
+
+span.onclick = function () {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+    });
+};
+
+document.addEventListener('DOMContentLoaded', function () {
+    var headerLink = document.getElementById("section2");
+    headerLink.addEventListener('click', function (event) {
+        event.preventDefault(); // prevent the default behavior of jumping to the section
+
+        var section = document.querySelector(event.target.getAttribute('href'));
+        var scrollOffset = 130; // adjust this value according to your needs
+
+        window.scrollTo({
+            top: section.offsetTop - scrollOffset,
+            behavior: 'smooth' // adds smooth scrolling effect
+        });
+    });
+});
+document.addEventListener('DOMContentLoaded', function () {
+    var headerLink = document.getElementById("section3");
+    headerLink.addEventListener('click', function (event) {
+        event.preventDefault(); // prevent the default behavior of jumping to the section
+
+        var section = document.querySelector(event.target.getAttribute('href'));
+        var scrollOffset = 130; // adjust this value according to your needs
+
+        window.scrollTo({
+            top: section.offsetTop - scrollOffset,
+            behavior: 'smooth' // adds smooth scrolling effect
+        });
+    });
+});
+document.addEventListener('DOMContentLoaded', function () {
+    var headerLink = document.getElementById("section4");
+    headerLink.addEventListener('click', function (event) {
+        event.preventDefault(); // prevent the default behavior of jumping to the section
+
+        var section = document.querySelector(event.target.getAttribute('href'));
+        var scrollOffset = 130; // adjust this value according to your needs
+
+        window.scrollTo({
+            top: section.offsetTop - scrollOffset,
+            behavior: 'smooth' // adds smooth scrolling effect
+        });
+    });
+});
+
+//
+
+
